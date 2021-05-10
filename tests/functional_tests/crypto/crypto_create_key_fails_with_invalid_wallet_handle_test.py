649272d538e7a4e61ba69f4699e654573815f635
Wei Yao <wy95@njit.edu>
2021-05-10 13:34:58 -0400

    update crypto

---
+++ tests/functional_tests/crypto/crypto_create_key_fails_with_invalid_wallet_handle_test.py
@@ -0,0 +1,22 @@
+import pytest
+from indy import crypto
+
+from test_scripts.functional_tests.crypto.crypto_test_base \
+    import CryptoTestBase
+from utilities import common, utils
+
+
+class TestCryptoCreateKeyWithInvalidWalletHandle(CryptoTestBase):
+    @pytest.mark.asyncio
+    async def test(self):
+        # 1. Create wallet.
+        # 2. Open wallet.
+        self.wallet_handle = await common.create_and_open_wallet_for_steps(
+            self.steps, self.wallet_name, self.pool_name, credentials=self.wallet_credentials)
+
+        # 3. Create verkey with invalid wallet handle. Expected error = 200
+        self.steps.add_step("Create verkey with invalid wallet handle")
+        await utils.perform_with_expected_code(
+                                         self.steps, crypto.create_key,
+                                         self.wallet_handle + 9999, "{}",
+                                         expected_code=200)
